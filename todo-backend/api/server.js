299794f693cb8db0593efd7da4098fcf04508962
jonathan-portillo <54336208+jonathan-portillo@users.noreply.github.com>
2022-05-19 19:56:57 -0400

    auth and users router added to server

--- todo-backend/api/server.js
+++ todo-backend/api/server.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const server = express();
 
+const usersRouter = require("./users/users-router");
+const authRouter = require("../auth/auth-router");
+
 server.use(express.json());
+
+server.use("/auth", authRouter);
+server.use("/users", usersRouter);
 
 server.get("/", (req, res) => {
   res.json({ api: "server is up" });
