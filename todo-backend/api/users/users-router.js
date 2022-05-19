75800315f33dc24b4fcaf819cf4cd814d9d88d50
jonathan-portillo <54336208+jonathan-portillo@users.noreply.github.com>
2022-05-19 19:39:21 -0400

    users router

---
+++ todo-backend/api/users/users-router.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const users = require("./users-model");
+
+//get users
+router.get("/", (req, res) => {
+  users
+    .find()
+    .then((users) => {
+      res.status(200).json(users);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send(err);
+    });
+});
