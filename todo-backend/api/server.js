580284c2a7210ac5ff615772ede8394a817f0960
jonathan-portillo <54336208+jonathan-portillo@users.noreply.github.com>
2022-09-13 20:37:06 -0400

    added todoList router

--- todo-backend/api/server.js
+++ todo-backend/api/server.js
@@ -4,12 +4,14 @@ const server = express();
 const usersRouter = require("./users/users-router");
 const authRouter = require("../auth/auth-router");
 const titleRouter = require("../api/todo/todo-title/todo_title-router");
+const todoListRouter = require("../api/todo/toto-list/todo_list_router");
 
 server.use(express.json());
 
 server.use("/auth", authRouter);
 server.use("/users", usersRouter);
 server.use("/title", titleRouter);
+server.use("/todoList", todoListRouter);
 
 server.get("/", (req, res) => {
   res.json({ api: "server is up" });
