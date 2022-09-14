f1e078918f1dc0bfd5024c535dba7ddd9206d6a2
jonathan-portillo <54336208+jonathan-portillo@users.noreply.github.com>
2022-09-13 20:37:42 -0400

    fixed and added new models

--- todo-backend/api/todo/toto-list/todo_list_model.js
+++ todo-backend/api/todo/toto-list/todo_list_model.js
@@ -2,11 +2,21 @@ const db = require("../../../data/db-config");
 
 module.exports = {
   findAllTodoList,
-  // findTodoLisById,
+  findTodoListById,
+  addTodoList,
   // updateTodoList,
   // deleteTodoList
 };
-76;
+
 function findAllTodoList() {
   return db("todo_list");
+}
+
+function findTodoListById(id) {
+  return db("todo_list").where({ id }).first();
+}
+
+async function addTodoList(newTodoList) {
+  const [id] = await db("todo_list").insert(newTodoList, "id");
+  return findTodoListById(id);
 }
