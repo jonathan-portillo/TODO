5842f9e039a8d458ab0ce60e2bfb8ca6525e0ca6
jonathan-portillo <54336208+jonathan-portillo@users.noreply.github.com>
2023-06-22 18:31:57 -0400

    folder scafolding/setting up files and components

---
+++ todo-list/src/components/landing.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <div>
+      <p>Life is rough so lets GetItDone</p>
+      <p>Sign Up!</p>
+      <p>Already have an account?</p>
+      <p>Log In! </p>
+    </div>
+  );
+};
+
+export default Landing;
--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import thunk from "redux-thunk";
+import { legacy_createStore as createStore } from "redux";
+import { applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { todoReducer } from "./reducers/todoReducer";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const store = createStore(todoReducer, applyMiddleware(thunk));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>
+  </Router>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
